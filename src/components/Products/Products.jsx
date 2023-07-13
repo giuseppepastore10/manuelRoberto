30250762b8255fca70684520331af33e2c0a74e6
Giuseppe Pastore <giuseppe.pastore1017@gmail.com>
2023-07-13 11:18:05 +0100

    Adds the line as border

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -17,7 +17,7 @@ function Products({ heading, data,left,imgs }) {
 			<ProductsWrapper>
 				{data.map((product, index) => {
 					return (
-						<div style={{display:'flex',flexDirection:'column', alignItems:'center', margin: "1rem"}}>
+						<div style={{display:'flex',flexDirection:'column', alignItems:'center', }}>
 						<ProductsCard key={index}>
 							<div style={{display:'flex',width:"100%", padding: "3rem", justifyContent:'center',alignItems:'center'}}>
 								{product.icon}
@@ -27,12 +27,13 @@ function Products({ heading, data,left,imgs }) {
 						
 						<ProductsCard  key={index}>
 							
-							<ProductsInfo zeroMargin fontSize="clamp(1rem, 2.5vw, 1.2rem)" textAlign="center">
+							<ProductsInfo zeroMargin fontSize="clamp(1.5rem, 2.5vw, 1.5rem)" textAlign="center">
 								{product.title}
 								
 								
 								
 							</ProductsInfo>
+							<div style={{width:"100%", marginTop:"1rem", marginBottom:"1rem", border: "1px solid white"}}></div>
 						</ProductsCard>
 						<ProductsCard zeroMargin key={index}>
 							
